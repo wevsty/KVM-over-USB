a211b7b356967f58b97c4b635502327ce049422b
wevsty <ty@wevs.org>
2024-10-05 18:02:17 +0800

    Update error log format

--- client/usb_kvm_client.py
+++ client/usb_kvm_client.py
@@ -3,13 +3,10 @@ import os
 import sys
 import traceback
 
-ARGV_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-
-def error_log(msg):
-    with open(os.path.join(ARGV_PATH, "error.log"), "w") as f:
-        f.write(f"Error Occurred at {datetime.datetime.now()}:\n")
-        f.write(f"{msg}\n")
+def write_message(path: str, message: str):
+    with open(path, "a+") as f:
+        f.write(f"{message}\n")
 
 
 if __name__ == "__main__":
@@ -18,6 +15,10 @@ if __name__ == "__main__":
 
         return_code = main()
         sys.exit(return_code)
-    except Exception:
-        error_log(traceback.format_exc())
+    except Exception as error:
+        base_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+        log_path = os.path.join(base_path, "error.log")
+        write_message(log_path, f"Error occurred at: {datetime.datetime.now()}\n")
+        write_message(log_path, f"Exception message: {error}\n")
+        write_message(log_path, traceback.format_exc())
         sys.exit(1)
