b35e2fb2c4777b18f151422b492a3a186918b1be
wevsty <ty@wevs.org>
2024-10-05 20:06:40 +0800

    add -Recurse parameter

--- client/ui/ui_resource/regenerate_ui.ps1
+++ client/ui/ui_resource/regenerate_ui.ps1
@@ -2,8 +2,8 @@
 # pyside6-uic main.ui -o main_u./i.py
 
 $ui_path = (Get-Location).Path + "\*";
-$ui_files = Get-ChildItem -Path $ui_path -Include "*.ui";
-
+$ui_files = Get-ChildItem -Path $ui_path -Recurse -Include "*.ui";
+Write-Host "ui path: $ui_path";
 foreach ($ui_file in $ui_files){
     Write-Host "generate ui file: $ui_file";
     $ui_code_path = $ui_file.FullName.Replace(".", "_") + ".py";
