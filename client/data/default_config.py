df3e68992d29db7149710b5fa096608451d39a61
wevsty <ty@wevs.org>
2025-01-08 18:28:47 +0800

    update default config

--- client/data/default_config.py
+++ client/data/default_config.py
@@ -1,52 +1,55 @@
 MAIN_DEFAULT_CONFIG_DATA = """
-config_version: v1
+config_version: v2
+audio:
+  audio_device_in: None
+  audio_device_out: None
+  audio_support: false
+controller:
+  baud: 9600
+  port: auto
+mouse:
+  cursor_offset_x: 4
+  cursor_offset_y: 6
+  relative_speed: 0.6
+  report_freq: 60
+paste_board:
+  interval: 1
 ui:
+  fullscreen_tip: true
   quick_paste: true
-  fullscreen_state_alert: false
 video:
   auto_connect: false
   device: None
   format: None
   keep_aspect_ratio: true
-  resolution_x: 1920
-  resolution_y: 1080
-audio:
-  audio_device_in: auto
-  audio_device_out: auto
-  audio_support: false
+  resolution_x: 1280
+  resolution_y: 720
 video_record:
   encoding_bitrate: 10000000
   encoding_mode: ConstantQualityEncoding
   frame_rate: 0
   quality: HighQuality
-controller:
-  port: auto
-  baud: 9600
-mouse:
-  relative_speed: 0.6
-  report_freq: 60
-  cursor_offset_x: 4
-  cursor_offset_y: 6
-paste_board:
-  interval: 1
 shortcut_keys:
-  Ctrl+Alt+Del:
-    - ctrl_left
-    - alt_left
-    - delete
-  Ctrl+Shift+Esc:
-    - ctrl_left
-    - shift_left
-    - esc
+  Alt+Tab:
+  - alt_left
+  - tab
   Alt+F4:
-    - alt_left
-    - f4
+  - alt_left
+  - f4
+  Ctrl+Alt+Del:
+  - ctrl_left
+  - alt_left
+  - delete
+  Ctrl+Shift+Esc:
+  - ctrl_left
+  - shift_left
+  - esc
   Meta+L:
-    - win_left
-    - l
+  - win_left
+  - l
   Meta+R:
-    - win_left
-    - r
+  - win_left
+  - r
 
 """
 
--- client/project_info.py
+++ client/project_info.py
@@ -1,5 +1,5 @@
 VERSION_STRING = "v2024.12.31"
-CONFIG_VERSION_STRING = "v1"
+CONFIG_VERSION_STRING = "v2"
 
 if __name__ == "__main__":
     pass
